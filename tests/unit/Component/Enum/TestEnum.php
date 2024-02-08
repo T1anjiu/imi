9dbe8ec006b53a92ddc84a3f11570b7b95465536
Tianjiu <84212214+T1anjiu@users.noreply.github.com>
2024-02-08 21:25:19 +0800

    character replacement

--- tests/unit/Component/Enum/TestEnum.php
+++ tests/unit/Component/Enum/TestEnum.php
@@ -8,17 +8,17 @@ use Imi\Enum\BaseEnum;
 class TestEnum extends BaseEnum
 {
     /**
-     * @EnumItem(text="甲", other="a1")
+     * @EnumItem(text="A", other="a1")
      */
     const A = 1;
 
     /**
-     * @EnumItem(text="乙", other="b2")
+     * @EnumItem(text="B", other="b2")
      */
     const B = 2;
 
     /**
-     * @EnumItem(text="丙", other="c3")
+     * @EnumItem(text="C", other="c3")
      */
     const C = 3;
 }
